23985f8d6c61046a7b2f6d1b27896a948c5bd28d
arsyad z <travisschafer87@yahoo.com>
2021-12-09 01:09:51 +0800

    proj migr -m

---
+++ app/Models/ProjStages.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProjStages extends Model
+{
+    use HasFactory;
+}
